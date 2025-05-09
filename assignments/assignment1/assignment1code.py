72e22dc3eadd825e1c854a745fc79a047a4c72f9
MMVS <zeinny1997@gmail.com>
2025-05-09 13:50:11 -0600

    assignment 1 fix; changing and references to 592 to 692. Fixing and dates from previous course and year. Updated readme

--- assignments/assignment1/assignment1code.py
+++ assignments/assignment1/assignment1code.py
@@ -1,8 +1,8 @@
-# ENSF 592 Spring 2023
+# ENSF 692 Spring 2025
 # Assignment 1 Starter Code
 # Manuja Senanayake
 
 studentName = "Manuja Senanayake"  # Change the contents of this variable to your name (leave the quotation marks).
 
 print("Hello, World!")
-print("My name is " + studentName + " and I am a student in ENSF 592.")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
